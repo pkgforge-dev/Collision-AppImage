ecce5de5140bc665ed58ee847d1013e4fec17416
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-18 12:52:07 +0100

    Add `GTK_CLASS_FIX` for StartupWMClass fix on Wayland

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,8 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/dev.geopjr.Collision.svg
 export DESKTOP=/usr/share/applications/dev.geopjr.Collision.desktop
 export DEPLOY_OPENGL=1
-export STARTUPWMCLASS=collision # For Wayland, this is 'dev.geopjr.Collision', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+export STARTUPWMCLASS=collision
+export GTK_CLASS_FIX=1
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 quick-sharun /usr/bin/collision
